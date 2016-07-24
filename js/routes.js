3cf0a268c5e51f11245cdd0d358078e838370a86
jpaden12 <j.paden12@yahoo.com>
2016-07-24 12:42:38 -0400

    -

--- js/routes.js
+++ js/routes.js
@@ -20,7 +20,7 @@ app.config(function($routeProvider, $locationProvider) {
             })
         .when("/contact",
             {
-                templateUrl: "../views/ContactMe.html",
+                templateUrl: "ContactMe.html",
                 controller: "contactPage"
             })
         .otherwise({
